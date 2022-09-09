3c74929f1a322b2d08c156069f8a1a01cea01030
SantiRojas <104436569+SantiRojas02@users.noreply.github.com>
2022-09-09 00:32:57 -0300

    Equivalencia_pies.cs

---
+++ Equivalencia_Pies.cs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Equivalencia_Pies.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pies, metros, yardas;
+            
+            Console.Write("Ingrese distancia en pies: ");
+            int distancia = Convert.ToInt32(Console.ReadLine());
+            double pulgada = 2.54;
+            pies = 12 * distancia;
+            yardas = 3 * distancia;
+            pulgada = 2.54 * distancia;
+            metros = 100 * distancia;
+
+            Console.WriteLine("Equivalente en yardas: "+ yardas);
+            Console.WriteLine("Equivalente en pulgadas: "+ pulgada);
+            Console.WriteLine("Equivalente en metros: "+ metros);
+
+            Console.ReadKey();
+
+        }
+    }
+}
